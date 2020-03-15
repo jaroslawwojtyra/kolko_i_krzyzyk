a5d953f702f90ce4af674800aabdcce392f9f6a6
Jaroslaw Wojtyra <jaroslawwojtyra@gmail.com>
2020-03-15 14:26:34 +0100

    v. 0.06 - google material icons

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const column = 3;
 const row = 3 ;
 const symbols = ["X", "O"];
 let board = document.querySelector('#board');
-console.log(board);
 board.addEventListener('click', icon);
-function icon() {
-  console.log("testyyy");
+function icon(event) {
+  
 }
