994ef345665981ebcafc5230eb35be37e60c6e54
Jaroslaw Wojtyra <jaroslawwojtyra@gmail.com>
2020-03-15 15:59:05 +0100

    v.0.07 - tury

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-const actualTurn = 0;
-const column = 3;
-const row = 3 ;
-const symbols = ["X", "O"];
+let actualTurn = 1;
+const player1 = 'panorama_fish_eye';
+const player2 = 'close';
+
 let board = document.querySelector('#board');
 board.addEventListener('click', icon);
+
 function icon(event) {
-  
-}
+  let round = actualTurn % 2;
+  if (round === 1){
+    console.log(player1)
+  }
+    else {
+      console.log(player2)
+    }
+    actualTurn += 1;
+  }
