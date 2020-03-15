3c93999852814f3d942ff4250f7e8144864eb7a9
Jaroslaw Wojtyra <jaroslawwojtyra@gmail.com>
2020-03-15 14:02:31 +0100

    v.0.05 - klick

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-var actualTurn = 0;
-var column = 3;
-var row = 3 ;
-var symbols = ["X", "O"];
-var board == [];
+const actualTurn = 0;
+const column = 3;
+const row = 3 ;
+const symbols = ["X", "O"];
+let board = document.querySelector('#board');
+console.log(board);
+board.addEventListener('click', icon);
+function icon() {
+  console.log("testyyy");
+}
