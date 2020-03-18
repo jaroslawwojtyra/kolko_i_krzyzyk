3afa186b87434d20aeb93245853f9da493813fcb
Jaroslaw Wojtyra <jaroslawwojtyra@gmail.com>
2020-03-18 17:03:53 +0100

    v. 0.17 - koniec gry po 1 ruchu

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 let actualTurn = 1;
 const icon1 = 'panorama_fish_eye';
 const icon2 = 'close';
-const p1 = 'Kółko'
-const p2 = 'Krzyżyk'
+const p1 = 'Kółko';
+const p2 = 'Krzyżyk';
+const end = 'Koniec gry'
 let gameBoard = document.querySelector('#board');
 let playerName = document.querySelector('#playerName');
+let endGame = false;
 
 const board = [
         ['', '', ''],
         ['', '', ''],
         ['', '', ''],
+];
+
+const winCombinations = [
+    [0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
 ];
 
 // nasłuchiwanie kliknięcia na tablicy gry
@@ -27,21 +35,29 @@ function addIcon(iconName) {
 
 // zmiana nazwy aktualnego gracza
 function changeName(name) {
-  playerName.innerText = name;
+  if (playerName.innerText = end) {
+    return
+  }
+    else {
+      playerName.innerText = name;
+    }
 };
 
 // sprawdzenie czy wygrana
 function checkWin() {
-  
+  if (true) {
+    endGame=true;
+    changeName("Koniec gry");
+  }
 }
 
 // rozgrywka
 function play(event) {
   let round = actualTurn % 2;
-  if (event.target.innerText == ''){
+  if (event.target.innerText == '' && endGame == false){
     if (round === 1){
       addIcon(icon1);
-      // checkWin();
+      checkWin();
       changeName(p2);
     }
       else {
