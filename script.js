c04c01c64d82bc0ae01831adf5e604f651805c58
Jaroslaw Wojtyra <jaroslawwojtyra@gmail.com>
2020-03-18 16:42:05 +0100

    v. 0.15 - tylko jedna ikona

--- script.js
+++ script.js
@@ -3,38 +3,41 @@ const icon1 = 'panorama_fish_eye';
 const icon2 = 'close';
 const p1 = 'Kółko'
 const p2 = 'Krzyżyk'
-let board = document.querySelector('#board');
+let gameBoard = document.querySelector('#board');
 let playerName = document.querySelector('#playerName');
 
-const table = [
+const board = [
         ['', '', ''],
         ['', '', ''],
         ['', '', ''],
 ];
 
-board.addEventListener('click', icon);
+gameBoard.addEventListener('click', icon);
 
 function addIcon(iconName) {
-  const el = document.createElement("i");
-  el.innerText = iconName;
-  el.classList.add("material-icons");
-  const clickEl = document.querySelector(`#${event.target.id}`);
-  clickEl.appendChild(el);
-}
+    const el = document.createElement("i");
+    el.innerText = iconName;
+    el.classList.add("material-icons");
+    const clickEl = document.querySelector(`#${event.target.id}`);
+    clickEl.appendChild(el);
+    actualTurn += 1;
+
+};
 
 function changeName(name) {
   playerName.innerText = name;
-}
+};
 
 function icon(event) {
   let round = actualTurn % 2;
-  if (round === 1){
-    addIcon(icon1);
-    changeName(p2);
-  }
-    else {
-      addIcon(icon2);
-      changeName(p1);
+  if (event.target.innerText == ''){
+
+    if (round === 1){
+      addIcon(icon1);
+      changeName(p2);
     }
-    actualTurn += 1;
-  }
+      else {
+        addIcon(icon2);
+        changeName(p1);
+      };
+    }};
