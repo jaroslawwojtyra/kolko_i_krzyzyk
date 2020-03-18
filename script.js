333dc0eb555f7a206ee1575d10015484ad6013dd
Jaroslaw Wojtyra <jaroslawwojtyra@gmail.com>
2020-03-18 16:45:45 +0100

    v. 0.16 - komentarze do kodu + dodanie dunkcji sprawdzania wygranej

--- script.js
+++ script.js
@@ -12,8 +12,10 @@ const board = [
         ['', '', ''],
 ];
 
-gameBoard.addEventListener('click', icon);
+// nasłuchiwanie kliknięcia na tablicy gry
+gameBoard.addEventListener('click', play);
 
+// dodanie kółka lub krzyżyka
 function addIcon(iconName) {
     const el = document.createElement("i");
     el.innerText = iconName;
@@ -21,23 +23,30 @@ function addIcon(iconName) {
     const clickEl = document.querySelector(`#${event.target.id}`);
     clickEl.appendChild(el);
     actualTurn += 1;
-
 };
 
+// zmiana nazwy aktualnego gracza
 function changeName(name) {
   playerName.innerText = name;
 };
 
-function icon(event) {
+// sprawdzenie czy wygrana
+function checkWin() {
+  
+}
+
+// rozgrywka
+function play(event) {
   let round = actualTurn % 2;
   if (event.target.innerText == ''){
-
     if (round === 1){
       addIcon(icon1);
+      // checkWin();
       changeName(p2);
     }
       else {
         addIcon(icon2);
+        // checkWin();
         changeName(p1);
       };
     }};
