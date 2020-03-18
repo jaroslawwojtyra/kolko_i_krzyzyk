27ca0a85f7116419ea73cd4186b66254d4419e42
Jaroslaw Wojtyra <jaroslawwojtyra@gmail.com>
2020-03-18 13:12:46 +0100

    v. 0.1 - Dodawanie X i O

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 let actualTurn = 1;
 const player1 = 'panorama_fish_eye';
 const player2 = 'close';
-
 let board = document.querySelector('#board');
+
 board.addEventListener('click', icon);
 
+function addO() {
+  const el = document.createElement("i");
+  el.innerText = "panorama_fish_eye";
+  el.classList.add("material-icons");
+  const clickEl = document.querySelector(`#${event.target.id}`);
+  clickEl.appendChild(el);
+}
+function addX() {
+  const el = document.createElement("i");
+  el.innerText = "close";
+  el.classList.add("material-icons");
+  const clickEl = document.querySelector(`#${event.target.id}`);
+  clickEl.appendChild(el);
+}
 function icon(event) {
   let round = actualTurn % 2;
   if (round === 1){
-    console.log(player1)
+    addO();
   }
     else {
-      console.log(player2)
+    addX();
     }
     actualTurn += 1;
   }
