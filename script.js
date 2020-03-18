bc8a016e5168a197153703eb1c22425f39cdc9a9
Jaroslaw Wojtyra <jaroslawwojtyra@gmail.com>
2020-03-18 16:19:59 +0100

    v.0.14 - zmiana playera

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 let actualTurn = 1;
 const icon1 = 'panorama_fish_eye';
 const icon2 = 'close';
+const p1 = 'Kółko'
+const p2 = 'Krzyżyk'
 let board = document.querySelector('#board');
-cons
+let playerName = document.querySelector('#playerName');
+
+const table = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+];
 
 board.addEventListener('click', icon);
 
@@ -14,13 +22,19 @@ function addIcon(iconName) {
   clickEl.appendChild(el);
 }
 
+function changeName(name) {
+  playerName.innerText = name;
+}
+
 function icon(event) {
   let round = actualTurn % 2;
   if (round === 1){
     addIcon(icon1);
+    changeName(p2);
   }
     else {
       addIcon(icon2);
+      changeName(p1);
     }
     actualTurn += 1;
   }
