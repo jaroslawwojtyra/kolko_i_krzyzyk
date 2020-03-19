ab6259ed3c6d68dfcfb1d22b886267039c7f0a92
Jaroslaw Wojtyra <jaroslawwojtyra@gmail.com>
2020-03-19 15:06:12 +0100

    v.0.2 - sprawdzanie wygranej w poziomie

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const p2 = 'Krzyżyk';
 let gameBoard = document.querySelector('#board');
 let playerName = document.querySelector('#playerName');
 let resultText = document.querySelector('.result');
+const all_td = document.getElementsByTagName("td");
 
 const board = [
         ['', '', ''],
@@ -28,31 +29,20 @@ gameBoard.addEventListener('click', play);
 function addIcon(iconName) {
     const el = document.createElement("i");
     el.innerText = iconName;
-    el.classList.add("material-icons");
+    el.classList.add("material-icons", iconName);
     const clickEl = document.querySelector(`#${event.target.id}`);
     clickEl.appendChild(el);
     actualTurn += 1;
 };
 
-function addClass(p) {
-    event.target.classList.add(p);
-    console.log(event.target);
-}
-
-// sprawdzenie czy wygrana
-function checkWin() {
-  if (moveCounter > 3) {
-    endGame=true;
-    playerName.innerText = 'Koniec gry';
-    resultText.innerText = `Grę wygrało: ${p1}`;
-
-  }
-}
-
 // zmiana nazwy aktualnego gracza
-function changeName(name) {
+function changeName(name, winName) {
   if (endGame == false) {
       playerName.innerText = `Aktualny gracz: ${name}`;
+    }
+    else {
+      resultText.innerText = `Grę wygrało: ${winName}`;
+
     }
 };
 
@@ -62,15 +52,26 @@ function play(event) {
   if (event.target.innerText == '' && endGame == false){
     if (round === 1){
       addIcon(icon1);
-      addClass(p1);
-      checkWin();
-      changeName(p2);
-    }
+      checkWin(icon1);
+      changeName(p2, p1);
+      }
       else {
         addIcon(icon2);
-        addClass(p2);
-        checkWin();
-        changeName(p1);
+        checkWin(icon2);
+        changeName(p1, p2);
       };
       moveCounter ++;
     }};
+
+    // sprawdzenie czy wygrana
+    function checkWin(p) {
+      if (moveCounter > 3) {
+        //linie poziome - sprawdzenie
+        for (var i = 0; i < all_td.length; i = i + 3) {
+          if (all_td[i].innerText == p && all_td[i+1].innerText == p && all_td[i+2].innerText == p ) {
+            endGame=true;
+            playerName.innerText = 'Koniec gry';
+          };
+        };
+      };
+    };
